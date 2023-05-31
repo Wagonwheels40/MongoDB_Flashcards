7523597ec187df48bcd2656ad5f774ce8e9277e2
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-05-31 12:04:47 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const dbUri = "mongodb+srv://dkwagner40:Lmbas123@cluster0.pfvqylq.mongodb.net/flashcards";
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    app.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas', error);
+  });
+
+const flashcardSchema = new mongoose.Schema({
+  question: String,
+  answer: String,
+});
+
+const Flashcard = mongoose.model('Flashcard', flashcardSchema);
+
+// Create a new flashcard
+app.post('/flashcards', (req, res) => {
+  const { question, answer } = req.body;
+
+  const flashcard = new Flashcard({ question, answer });
+  flashcard.save()
+    .then(() => {
+      res.status(201).json(flashcard);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+// Get all flashcards
+app.get('/flashcards', (req, res) => {
+  Flashcard.find()
+    .then((flashcards) => {
+      res.json(flashcards);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+// Delete a flashcard
+app.delete('/flashcards/:id', (req, res) => {
+  const { id } = req.params;
+
+  Flashcard.findByIdAndDelete(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+
+// As it stands, it should just show you an empty array on localhost:3000/Flashcards
